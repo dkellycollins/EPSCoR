ea60bbd4fff593ce370994c8e3f25e7e5df7b974
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-04-15 15:12:37 -0500

    Recreated home page.

--- EPSCoR/App_Start/BundleConfig.cs
+++ EPSCoR/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace EPSCoR
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/style.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
