862f1db3cc12cc040e65b1c8a28fa70b262a10c6
Devin Kelly-Collins <dkellycollins@gmail.com>
2013-05-24 16:50:47 -0500

    Added in the fineuploader files.

--- EPSCoR/App_Start/BundleConfig.cs
+++ EPSCoR/App_Start/BundleConfig.cs
@@ -38,6 +38,12 @@ namespace EPSCoR
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            bundles.Add(new StyleBundle("~/Content/fineuploader").Include(
+                        "~/Content/fineuploader-{version}.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/fineuploader").Include(
+                        "~/Scripts/FineUploader/jquery.fineuploader-{version}.js"));
         }
     }
 }
